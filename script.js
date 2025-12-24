51eb1d81c7c686886138f9a41d9564128d21a778
kaito-creator <deathnoteraluca@gmail.com>
2025-12-24 15:44:59 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+function RolarD4() {
+    
+    const numeroaleatorio4 = Math.floor(Math.random() *4) + 1;
+    const elementoresultado = document.getElementById("resultado4")
+    elementoresultado.textContent = alert(`${numeroaleatorio4}!`);;
+}
+function RolarD6() {
+    const numeroaleatorio6 = Math.floor(Math.random() *6) + 1;
+    const elementoresultado = document.getElementById("resultado6")
+    elementoresultado.textContent = alert(`${numeroaleatorio6}!`);;
+}
+function RolarD8() {
+    const numeroaleatorio8 = Math.floor(Math.random() *8) + 1;
+    const elementoresultado = document.getElementById("resultado8")
+    elementoresultado.textContent = alert(`${numeroaleatorio8}!`);;
+}
+function RolarD10() {
+    const numeroaleatorio10 = Math.floor(Math.random() *10) + 1;
+    const elementoresultado = document.getElementById("resultado10")
+    elementoresultado.textContent = alert(`${numeroaleatorio10}!`);;
+}
+function RolarD12() {
+    const numeroaleatorio12 = Math.floor(Math.random() *12) + 1;
+    const elementoresultado = document.getElementById("resultado12")
+    elementoresultado.textContent = alert(`${numeroaleatorio12}!`);;
+}
+
+function RolarD20() {
+    const numeroaleatorio20 = Math.floor(Math.random() *20) + 1;
+    const elementoresultado = document.getElementById("resultado20")
+    elementoresultado.textContent = alert(`${numeroaleatorio20}!`);;
+}
+function RolarD100() {
+    const numeroaleatorio100 = Math.floor(Math.random() *100) + 1;
+    const elementoresultado = document.getElementById("resultado100")
+    elementoresultado.textContent = alert(`${numeroaleatorio100}!`);;}
